279b2b02542d53e2654a20dc9c94b6eb7a2c053d
Guzman <guzmanvigliecca@gmail.com>
2020-09-07 22:13:10 -0400

    Add colors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,15 +10,15 @@ function App() {
   return (
     <div className="App">
       <Container textAlign='center' style={{ marginTop: "2em" }}>
-        <Header as='h1' color='blue'>
+        <Header as='h1' style={{ color: "#A20021" }}>
           <Icon name='law' />
           Human Rights & Global Economy
         </Header>
-        <Header as='h3'>
+        <Header as='h3' style={{ color: "#080357" }}>
            Migrant Rights / Family separation in the U.S. <br/>
            Survey
         </Header>
-        <Header as='h4'>
+        <Header as='h4' style={{ marginBottom: "5em", color: "#A20021" }}>
           Amelia Caramadre, Carly Gleichenhaus & Florencia Ces
         </Header>
       </Container>
